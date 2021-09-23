62bfc639cd5ea1dd8b98f68e9cc2bd3b811e0b88
Agung Dwi Sahputra <91104004+AgungDwiSahputra@users.noreply.github.com>
2021-09-23 11:18:34 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Kelompok Basis Data KJ003</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/offcanvas-navbar/">
+
+    <!-- Bootstrap core CSS -->
+<link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<meta name="theme-color" content="#7952b3">
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    
+    <!-- Custom styles for this template -->
+    <link href="https://getbootstrap.com/docs/5.0/examples/offcanvas-navbar/offcanvas.css" rel="stylesheet">
+  </head>
+  <body class="bg-light">
+<h1 class="h2 mb-5 text-black lh-1 text-center text-decoration-underline">LIST KELOMPOK MATA KULIAH BASIS DATA</h1>
+<main class="container">
+  <div class="d-flex align-items-center p-3 text-white bg-primary rounded shadow-sm">
+    <div class="lh-1">
+      <h1 class="h4 mb-0 text-white lh-1">Sudah Dapat Kelompok</h1>
+      <small>Universitas Esa Unggul</small>
+    </div>
+  </div>
+
+<div class="container mt-0 pt-0">
+	<div class="row">
+		<div class="col">
+			  <div class="my-3 p-3 bg-body rounded shadow-sm">
+			    <h6 class="border-bottom pb-2 mb-0">KELOMPOK 1</h6>
+			    <div class="d-flex text-muted pt-3">
+			      <h1 class="h6 mx-2 text-black lh-1">01</h1>
+			      <p class="pb-3 mb-0 small lh-sm border-bottom">
+			        <strong class="d-block text-gray-dark">Agung Dwi Sahputra</strong>
+			      </p>
+			    </div>
+			    <small class="d-block text-end mt-3">
+			      Total : 7
+			    </small>
+			  </div>
+		</div>
+		
+	</div>
+</div>
+
+  <div class="d-flex align-items-center p-3 mt-5 text-white bg-warning rounded shadow-sm">
+    <div class="lh-1">
+      <h1 class="h4 mb-0 text-white lh-1">Belum Dapat Kelompok</h1>
+      <small>Universitas Esa Unggul</small>
+    </div>
+  </div>
+
+  <div class="container mt-0 pt-0">
+	<div class="row">
+		<div class="col">
+			  <div class="my-3 p-3 bg-body rounded shadow-sm">
+			    <h6 class="border-bottom pb-2 mb-0">Total : 67</h6>
+			    <div class="d-flex text-muted pt-3">
+			      <h1 class="h6 mx-2 text-black lh-1">01</h1>
+			      <p class="pb-3 mb-0 small lh-sm border-bottom">
+			        <strong class="d-block text-gray-dark">Agung Dwi Sahputra</strong>
+			      </p>
+			    </div>
+			  </div>
+		</div>
+		
+	</div>
+</div>
+
+</main>
+    <script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <script src="https://getbootstrap.com/docs/5.0/examples/offcanvas-navbar/offcanvas.js"></script>
+  </body>
+</html>
